d7ec229998db9c382661db2b96efe8c3fdbbac21
Rémy Fèvre <r.fevre184@gmail.com>
2015-11-26 13:55:54 +0100

    fin de tout les exos

--- Exo2/echo-serveur-2.pl
+++ Exo2/echo-serveur-2.pl
@@ -1,22 +1,28 @@
 #!/usr/bin/perl
-
 use Socket;
+use POSIX ":sys_wait_h";
+
 socket (SERVEUR, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
 setsockopt (SERVEUR, SOL_SOCKET, SO_REUSEADDR, 1);
 $mon_adresse = sockaddr_in ("7777", INADDR_ANY);
 bind(SERVEUR, $mon_adresse) || die ("bind");
+listen (SERVEUR, SOMAXCONN) || die ("listen");
 
 while (true) {
-    listen (SERVEUR, SOMAXCONN) || die ("listen");
     accept (CLIENT, SERVEUR) || die ("accept");
-    select (CLIENT);
 
-    while (<CLIENT>){
+    if(fork() == 0) {
 	CLIENT->autoflush(1);
-	print $_;
+	while (<CLIENT>){
+	    print CLIENT "serveur : $_";
+	}
+	exit 0;
     }
 
     close (CLIENT);
+    do{
+	$wPid = waitpid(-1,WNOHANG);
+    }while($wPid > 0);
 }
 
 close (SERVEUR);
